9bebecca48681fab1f296f316ecb3222c4b4add9
Vlada <vladochka.vb@gmail.com>
2019-06-17 09:31:59 +1200

    start

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require("webpack");
+module.exports = {
+   mode: 'development',
+    module: {
+     rules: [
+               {
+                  test: /\.scss$/,
+                  use: [
+                        "style-loader", // creates style nodes from JS strings
+                        "css-loader", // translates CSS into CommonJS
+                        "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                  ],
+               },
+               {
+                  test: /\.script\.js$/,
+                  use: [
+                    {
+                      loader: 'script-loader',
+                      options: {
+                        sourceMap: true,
+                      },
+                    },
+                  ]
+                },
+
+                {
+                  test: /\.(png|jpe?g|gif|jpg)$/,
+                  use: [
+                    {
+                      loader: 'file-loader',
+                      options: {
+                      name: '[path][name].[ext]',
+                      },
+                    },
+                  ]
+                  },
+
+                  {
+                  test: /\.(png|jpg|gif)$/i,
+                  use: [
+                     {
+                        loader: 'url-loader',
+                        options: {
+                        limit: 8000000000,
+                        },
+                     },
+                  ],
+                  },  
+            ]
+    },
+    plugins:[new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),]
+    
+};
